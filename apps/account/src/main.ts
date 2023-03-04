51a3d201ce51a9915595561702e3e89a44b554db
Aikanysh Satymkulova <aikasat95@gmail.com>
2023-03-04 19:30:02 +0600

    chore(account): remove comment

--- apps/account/src/main.ts
+++ apps/account/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
@@ -14,9 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(
-    `🚀 Accounts is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Accounts is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
