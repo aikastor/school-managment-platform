f2489db97440ad3b738de0890f7d759fb4fef52b
Aikanysh Satymkulova <aikasat95@gmail.com>
2023-03-04 16:51:59 +0600

    feat(account): add config setup

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,8 +1,17 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthModule } from './auth/auth.module';
+import { getMongoCongfig } from './congifs';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'envs/.account/env' }),
+    MongooseModule.forRootAsync(getMongoCongfig()),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ apps/account/src/app/congifs/index.ts
@@ -0,0 +1,1 @@
+export { getMongoCongfig } from './mongo.config';
